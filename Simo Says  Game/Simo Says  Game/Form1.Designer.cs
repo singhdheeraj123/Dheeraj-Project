9e92d551f44e28d6cc14e8a0edd183266c256080
singhdheeraj123 <142880933+singhdheeraj123@users.noreply.github.com>
2025-05-18 16:51:31 +0530

    Add files via upload

---
+++ Simo Says  Game/Simo Says  Game/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Simo_Says__Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Gametimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Simo Says Game";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(102, 426);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(366, 30);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Click on 3 Block in the same sequence";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(188, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonClickEvent);
+            // 
+            // Gametimer
+            // 
+            this.Gametimer.Interval = 20;
+            this.Gametimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 511);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Simo Says Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer Gametimer;
+    }
+}
+
---
+++ Simo Says  Game/Simo Says  Game/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Simo_Says__Game
+{
+    public partial class Form1 : Form
+    {
+        int blocksX = 160;
+        int blocksY = 80;
+        int score = 0;
+        int level = 3;
+
+        List<PictureBox> pictureBoxes = new List<PictureBox>();
+        List<PictureBox> chosenBoxes = new List<PictureBox>();
+        Random rand = new Random();
+
+        Color temp;
+
+        int index = 0;
+        int tries = 0;
+        int timeLimit = 0;
+        bool selectingColors = false;
+
+        string correctOrder = string.Empty;
+        string playerOrder = string.Empty;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetUpBlocks();
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            if (selectingColors == true)
+            {
+                timeLimit++;
+
+                switch (timeLimit)
+                {
+                    case 10:
+                        temp = chosenBoxes[index].BackColor;
+                        chosenBoxes[index].BackColor = Color.White;
+                        break;
+                    case 20:
+                        chosenBoxes[index].BackColor = temp;
+                        break;
+                    case 30:
+                        chosenBoxes[index].BackColor = Color.White;
+                        break;
+                    case 40:
+                        chosenBoxes[index].BackColor = temp;
+                        break;
+                    case 50:
+                        if (index < chosenBoxes.Count - 1)
+                        {
+                            index++;
+                            timeLimit = 0;
+                        }
+                        else
+                        {
+                            selectingColors = false;
+                        }
+                        break;
+
+                }
+            }
+                if (tries >= level)
+                {
+                    
+                    if (correctOrder == playerOrder)
+                    {
+                        tries = 0;
+                        Gametimer.Stop();
+                        MessageBox.Show("Well done, you got them correctly", "moo Says: ");
+                        score++;
+                    }
+                    else
+                    {
+                        tries = 0;
+                        Gametimer.Stop();
+                        MessageBox.Show("Your gusses did not match,try again", "moo Says:");
+
+                    }
+                }
+
+
+                lblInfo.Text = "Click on " + level + "blocks in the same sequence";
+
+
+
+            }
+
+        
+
+            private void ButtonClickEvent(object sender, EventArgs e)
+            {
+            if (score == 3 && level < 7)
+            {
+                level++;
+                score = 0;
+            }
+
+            correctOrder = string.Empty;
+            playerOrder = string.Empty;
+            chosenBoxes.Clear();
+            chosenBoxes = pictureBoxes.OrderBy(x => rand.Next()).Take(level).ToList();
+
+            for (int i = 0; i < chosenBoxes.Count; i++)
+            {
+                correctOrder += chosenBoxes[i].Name + " ";
+            }
+
+            foreach (PictureBox x in pictureBoxes)
+            {
+                x.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+            }
+            Debug.WriteLine(correctOrder);
+            index = 0;
+            timeLimit = 0;
+            selectingColors = true;
+            Gametimer.Start();
+
+
+
+
+
+            }
+
+        private void SetUpBlocks()
+        {
+            for (int i = 1; i < 17; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Name = "Pic_" + i;
+                newPic.Height = 60;
+                newPic.Width = 60;
+                newPic.BackColor = Color.Black;
+                newPic.Left = blocksX;
+                newPic.Top = blocksY;
+                newPic.Click += ClickOnPictureBox;
+
+                if (i == 4 || i == 8 || i == 12)
+                {
+                    blocksY += 65;
+                    blocksX = 160;
+                }
+                else
+                {
+                    blocksX += 65;
+                }
+
+                this.Controls.Add(newPic);
+                pictureBoxes.Add(newPic);
+            }
+
+
+        }
+
+        private void ClickOnPictureBox(object sender, EventArgs e)
+        {
+            if (!selectingColors && chosenBoxes.Count > 1)
+            {
+                PictureBox temp = sender as PictureBox;
+                temp.BackColor = Color.Black;
+                playerOrder += temp.Name + " ";
+                Debug.WriteLine(playerOrder);
+                tries++;
+            }
+            else
+            {
+                return ;
+            }
+
+        }
+    }
+}
